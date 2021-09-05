f8146aa273aa9bbcbc04164cec9690675d0b9b38
srai5 <srai5@hawk.iit.edu>
2021-09-05 09:47:27 -0500

    initial import for Lab 2

---
+++ src/main/java/edu/iit/sat/itmd4515/srai5/srai5lab2/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.iit.sat.itmd4515.srai5.srai5lab2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/edu/iit/sat/itmd4515/srai5/srai5lab2/HelloResource.java
@@ -0,0 +1,14 @@
+package edu.iit.sat.itmd4515.srai5.srai5lab2;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
